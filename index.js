a184ed441555261432e30a4756565fc3c646fcab
quynhnam1123 <chloevandypun0suy@hotmail.com>
2024-05-08 19:39:14 +0700

    update sdk commit

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-function partitionLabels(s) {
-  const last = new Array(26);
-  for (let i = 0; i < s.length; i++) {
-    last[s.charCodeAt(i) - "a".charCodeAt(0)] = i;
+function findSubstring(s, words) {
+  if (s.length === 0 || words.length === 0) return [];
+  const wordMap = new Map();
+  for (const word of words) {
+    wordMap.set(word, (wordMap.get(word) || 0) + 1);
   }
-  let anchor = 0;
-  let j = 0;
+  const wordLength = words[0].length;
+  const totalLength = words.length * wordLength;
   const result = [];
-  for (let i = 0; i < s.length; i++) {
-    j = Math.max(j, last[s.charCodeAt(i) - "a".charCodeAt(0)]);
-    if (i === j) {
-      result.push(i - anchor + 1);
-      anchor = i + 1;
+  for (let i = 0; i <= s.length - totalLength; i++) {
+    const substring = s.substring(i, i + totalLength);
+    if (isValid(substring, new Map(wordMap))) {
+      result.push(i);
     }
   }
   return result;
+  function isValid(substring, wordMap) {
+    const wordCount = new Map();
+    for (let i = 0; i < substring.length; i += wordLength) {
+      const word = substring.substring(i, i + wordLength);
+      wordCount.set(word, (wordCount.get(word) || 0) + 1);
+    }
+    for (const [key, value] of wordMap) {
+      if ((wordCount.get(key) || 0) !== value) return false;
+    }
+    return true;
+  }
 }
