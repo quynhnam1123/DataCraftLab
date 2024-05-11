89673dd293ee270cf411cb2be0a026bb7d9fe696
quynhnam1123 <chloevandypun0suy@hotmail.com>
2024-05-12 03:28:39 +0700

    aptos commit

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-// Alice should have the initial balance minus tranfer amount
-if (aliceBalanceAfter[0].amount !== ALICE_INITIAL_BALANCE - TRANSFER_AMOUNT)
-  throw new Error("Alice's balance after transfer is incorrect");
-
-// Sponsor should have the initial balance minus gas
-if (sponsorBalanceAfter[0].amount >= SPONSOR_INITIAL_BALANCE)
-  throw new Error("Sponsor's balance after transfer is incorrect");
+const selectionSortRecursive = (arr, start = 0) => {
+  if (start >= arr.length - 1) {
+    return arr;
+  }
+  let minIndex = start;
+  for (let i = start + 1; i < arr.length; i++) {
+    if (arr[i] < arr[minIndex]) {
+      minIndex = i;
+    }
+  }
+  if (minIndex !== start) {
+    [arr[start], arr[minIndex]] = [arr[minIndex], arr[start]];
+  }
+  return selectionSortRecursive(arr, start + 1);
+};
