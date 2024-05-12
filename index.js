97e490c9dc8e7a997d5d3fd977bd8bb7e6ef16d7
quynhnam1123 <chloevandypun0suy@hotmail.com>
2024-05-12 16:08:00 +0700

    demo project commit

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-const selectionSortRecursive = (arr, start = 0) => {
-  if (start >= arr.length - 1) {
-    return arr;
-  }
-  let minIndex = start;
-  for (let i = start + 1; i < arr.length; i++) {
-    if (arr[i] < arr[minIndex]) {
-      minIndex = i;
-    }
-  }
-  if (minIndex !== start) {
-    [arr[start], arr[minIndex]] = [arr[minIndex], arr[start]];
-  }
-  return selectionSortRecursive(arr, start + 1);
-};
+function sumArray(arr) {
+  return arr.reduce((acc, curr) => acc + curr, 0);
+}
