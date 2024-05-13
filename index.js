64ba67eaf4ab4f9189a308b5b829fb730d83a56e
quynhnam1123 <chloevandypun0suy@hotmail.com>
2024-05-13 10:45:40 +0700

    aptos project

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-function sumArray(arr) {
-  return arr.reduce((acc, curr) => acc + curr, 0);
+function rotateRight(head, k) {
+  if (!head || k === 0) return head;
+  let length = 1;
+  let tail = head;
+  while (tail.next) {
+    length++;
+    tail = tail.next;
+  }
+  k = k % length;
+  if (k === 0) return head;
+  let newTail = head;
+  for (let i = 1; i < length - k; i++) newTail = newTail.next;
+  const newHead = newTail.next;
+  newTail.next = null;
+  tail.next = head;
+  return newHead;
 }
